9a66bfc6c586941ca2b0b9557c4c70c3d043073e
YX <503203440@qq.com>
2019-08-02 16:27:22 +0800

    ***

--- src/main/java/com/yx/tq/task/controller/AddTaskQueueController.java
+++ src/main/java/com/yx/tq/task/controller/AddTaskQueueController.java
@@ -2,6 +2,7 @@ package com.yx.tq.task.controller;
 
 import com.yx.tq.task.component.DataContainer;
 import com.yx.tq.task.entity.Mail;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,22 +21,26 @@ public class AddTaskQueueController {
      * 访问此接口将会向组件类中的任务队列写入9999个待处理的任务，处理任务的线程池会处理此任务
      * @return
      */
-    @RequestMapping("/addTask")
-    public Object addTask(){
+    @RequestMapping("/addTask/{n}")
+    public Object addTask(@PathVariable("n") Integer n){
 
-        for (int i = 0; i < 9999; i++) {
+        if(n!=null){
 
-            Mail mail =new Mail();
-            mail.setName("NO."+(i+1));
-            mail.setSendTime(new Date());
+            for (int i = 0; i < n; i++) {
 
-            //如果不包含则加入到mails
-            if(!DataContainer.mail.contains(mail)){
-                DataContainer.mail.add(mail);
+                Mail mail =new Mail();
+                mail.setName("NO."+(i+1));
+                mail.setSendTime(new Date());
+
+                //如果不包含则加入到mails
+                if(!DataContainer.mail.contains(mail)){
+                    DataContainer.mail.add(mail);
+                }
+
             }
 
         }
-        return "000";
+        return n;
     }
 
 
